56fb48bc6ea988704a245bfb08110da199c0d0ba
yoponchik <57009810+yoponchik@users.noreply.github.com>
2022-03-11 13:37:12 +0900

    Create Dictionary.py

---
+++ Data Structures/Dictionary.py
@@ -0,0 +1,11 @@
+
+
+
+dictionary = {
+        'key': {                                  #the key or the input (in this case there are two)
+            'array': [13, 11, 10, 7, 4, 3, 1, 0],
+            'int': 7
+        }, 
+        'value': 3                                 #value or the output
+    }
+
